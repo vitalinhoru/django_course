fdd65b5548e1541ace33b3ac8f039c18cd103bf3
vitalinho <kirillovitaly@mail.ru>
2024-04-10 19:31:35 +0300

    app Blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from blog.apps import BlogConfig
 from blog.views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView
@@ -6,7 +7,7 @@ from blog.views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateV
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='blog_list'),
+    path('', cache_page(60)(BlogListView.as_view()), name='blog_list'),
     path('create/', BlogCreateView.as_view(), name='blog_create'),
     path('view/<int:pk>/', BlogDetailView.as_view(), name='blog_view'),
     path('edit/<int:pk>/', BlogUpdateView.as_view(), name='blog_edit'),
